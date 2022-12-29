2b95f891b34597cc6daac43d66d7c4d48e47b629
vogitcode <vogitcode@gmail.com>
2022-12-29 23:48:49 +0700

    update character player commit

---
+++ Assets/Characters/Player/SwordAttack.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+    Collider2D swordCollider;
+    Vector2 rightAttackOffset;
+
+    private void Start()
+    {
+        swordCollider = GetComponent<Collider2D>();
+        rightAttackOffset = transform.position;
+    }
+
+    public void AttackRight()
+    {
+        swordCollider.enabled = true;
+        transform.position = rightAttackOffset;
+    }
+
+    public void AttackLeft()
+    {
+        swordCollider.enabled = true;
+        transform.position = new Vector3(rightAttackOffset.x * -1, rightAttackOffset.y);
+    }
+
+    public void StopAttack()
+    {
+        swordCollider.enabled = false;
+    }
+}
