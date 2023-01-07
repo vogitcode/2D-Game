32256934ade1372b4e210ab3b85136d0e2d31dda
KiHaRu <102032780+K1HaRu@users.noreply.github.com>
2023-01-07 10:46:28 +0700

    Enemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void TakeDamage(float damage)
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
